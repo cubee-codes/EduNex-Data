e8d16ab0322b9e38318ca21a765fe47fc14c3d58
cubee-codes <manojsonpethkar@gmail.com>
2026-05-01 12:43:40 +0530

    Update mobile_app.py

--- mobile_app.py
+++ mobile_app.py
@@ -23,7 +23,7 @@ UPLOADS_DIR = os.path.join(ASSETS_DIR, "uploads")
 # ---------------------------------------------------------
 load_dotenv() 
 
-API_KEY = os.getenv("GITHUB_API_KEY") 
+API_KEY = "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"
 MODEL_NAME = "gpt-4o-mini" 
 
 CLOUD_DATA = {
